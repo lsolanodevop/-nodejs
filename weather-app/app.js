8b4002f1487dbf266ddc1a76b5e632de7813d288
lsolanodevop <leonardo.solano.hw@gmail.com>
2021-06-20 01:21:11 -0300

    Creando carpeta utils y exportando funcion

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const postman = require('postman-request');
 const prompt = require("prompt-sync")();
+const geocode = require("./utils/geocode")
 const key = "9747e2502dfcc4d13aa8de971a181e18";
 const key2 = "pk.eyJ1IjoibHNvbGFubyIsImEiOiJja3EwNzR3d2UwMjF3Mm9wNGV3dnk0OTdhIn0.omsNVLBXj38YqsZn8RU21g";
 const url = "http://api.weatherstack.com/current?access_key=9747e2502dfcc4d13aa8de971a181e18&query=Montevideo";
@@ -36,29 +37,7 @@ const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Caricuao.json?ac
 // }); 
 //encodeURIComponent funcion para poder buscar strings con caracteres especiales y que no explote el peo
 const location = prompt("Tipe the city you're looking for ");
-const geocode = (address,callback) => 
-{
-   const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +".json?access_token=pk.eyJ1IjoibHNvbGFubyIsImEiOiJja3EwNzR3d2UwMjF3Mm9wNGV3dnk0OTdhIn0.omsNVLBXj38YqsZn8RU21g&limit=1";
-    // console.log(url);
-    postman({url:url,json:true}, (error , response) =>
-    {
-        if(error)
-        {
-            callback("Unable to send the request", undefined);
-        }else if(response.body.features.length === 0)
-        {
-            callback("Unable to find location. Try another search",undefined);
-        }else 
-        {
-            callback(undefined,
-                {
-                    latitude: response.body.features[0].center[0],
-                    longitude: response.body.features[0].center[1],
-                    location: response.body.features[0].place_name
-                });
-        }
-    });
-}
+
 geocode(location,(error,data) => {
 console.log("Error",error);
 console.log("Data",data);
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,27 @@
+const postman = require("postman-request");
+
+const geocode = (address,callback) => 
+{
+   const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +".json?access_token=pk.eyJ1IjoibHNvbGFubyIsImEiOiJja3EwNzR3d2UwMjF3Mm9wNGV3dnk0OTdhIn0.omsNVLBXj38YqsZn8RU21g&limit=1";
+    // console.log(url);
+    postman({url:url,json:true}, (error , response) =>
+    {
+        if(error)
+        {
+            callback("Unable to send the request", undefined);
+        }else if(response.body.features.length === 0)
+        {
+            callback("Unable to find location. Try another search",undefined);
+        }else 
+        {
+            callback(undefined,
+                {
+                    latitude: response.body.features[0].center[0],
+                    longitude: response.body.features[0].center[1],
+                    location: response.body.features[0].place_name
+                });
+        }
+    });
+}
+
+module.exports = geocode;
